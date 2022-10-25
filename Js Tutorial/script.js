c88db5704a83d0c45db16ea0b4e7a84283be21fc
Student <student@iMac-Astrum-155.local>
2022-10-25 16:04:13 +0500

    yelp

---
+++ Js Tutorial/script.js
@@ -0,0 +1,23 @@
+// let age = 16;
+// let myName = "Rozimurod";
+// let bool = false;
+// let number;
+// //console.log(number);
+// //console.log(a);
+
+// // object
+
+let person = {
+  name: "Rozimurod",
+  age: "16",
+  isMarried: false,
+};
+console.log(person);
+
+//array
+let arr = ["Phone", "Mouse", "Soap"];
+console.log(arr);
+
+let x = 5;
+let y = 6;
+console.log(x + 7);
